b88b9a034dfe912a98e0834732a3f7194f26ae81
Sergei Gladkov <kelebrik@gmail.com>
2026-07-14 10:38:23 +0300

    fix(web): restore risk matrix

--- apps/web/src/pages/ProjectRaidPage.tsx
+++ apps/web/src/pages/ProjectRaidPage.tsx
@@ -1,6 +1,7 @@
 import { Plus, X } from "lucide-react";
 import { useState } from "react";
 import { ProjectRaidRegister } from "./ProjectRaidRegister";
+import { ProjectRaidMatrixCard } from "./ProjectRaidMatrixCard";
 import { ProjectRaidSidePanel } from "./ProjectRaidSidePanel";
 import { useFocusTrap } from "../hooks/useFocusTrap";
 
@@ -24,8 +25,9 @@ export function ProjectRaidPage() {
                         <Plus size={15} /> Новая запись
                       </button>
                   </div>
-                  <div className="raid-board raid-board-wide">
+                  <div className="raid-board raid-board-matrix">
                     <ProjectRaidRegister />
+                    <ProjectRaidMatrixCard />
                   </div>
                   {showCreatePanel && (
                     <div className="drawer-backdrop" onMouseDown={() => setShowCreatePanel(false)}>
